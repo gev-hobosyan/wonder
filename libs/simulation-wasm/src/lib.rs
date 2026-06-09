c5e72f46e0b54927e77f6542ca35e497e84cc135
gev-hobosyan <hobosyangevorg2008@gmail.com>
2026-06-09 18:55:34 +0400

    Our animals now on BIG SCREENS:)

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,6 +1,55 @@
+use lib_simulation as sim;
+use rand::{prelude::*, rng};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn just_checking() -> String {
-    "Check successful".into()
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = rng();
+        let sim = sim::Simulation::random(&mut rng);
+
+        Self { rng, sim }
+    }
+
+    pub fn world(&self) -> World {
+        World::from(self.sim.world())
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
+pub struct World {
+    #[wasm_bindgen(getter_with_clone)]
+    pub animals: Vec<Animal>,
+}
+
+#[wasm_bindgen]
+#[derive(Debug, Clone)]
+pub struct Animal {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl From<&sim::World> for World {
+    fn from(world: &sim::World) -> Self {
+        let animals = world.animals().iter().map(Animal::from).collect();
+
+        Self { animals }
+    }
+}
+
+impl From<&sim::Animal> for Animal {
+    fn from(animal: &sim::Animal) -> Self {
+        Self {
+            x: animal.position().x,
+            y: animal.position().y,
+        }
+    }
 }
