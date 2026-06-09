71888f3ab8fbfc682853fc33882b9ef4efde1f5f
gev-hobosyan <hobosyangevorg2008@gmail.com>
2026-06-09 14:50:23 +0400

    Simple Simulation setup.

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,14 +1,85 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use nalgebra as na;
+use rand::{Rng, RngExt};
+
+#[derive(Debug)]
+pub struct Simulation {
+    world: World,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub struct World {
+    animals: Vec<Animal>,
+    foods: Vec<Food>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug)]
+pub struct Animal {
+    position: na::Point2<f32>,
+    rotation: na::Rotation2<f32>,
+    speed: f32,
+}
+
+#[derive(Debug)]
+pub struct Food {
+    position: na::Point2<f32>,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn Rng) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+
+    pub fn world(&self) -> &World {
+        &self.world
+    }
+}
+
+impl World {
+    pub fn random(rng: &mut dyn Rng) -> Self {
+        let animals = (0..40).map(|_| Animal::random(rng)).collect();
+
+        let foods = (0..60).map(|_| Food::random(rng)).collect();
+
+        Self { animals, foods }
+    }
+
+    pub fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+
+    pub fn foods(&self) -> &[Food] {
+        &self.foods
+    }
+}
+
+impl Animal {
+    pub fn random(rng: &mut dyn Rng) -> Self {
+        Self {
+            position: rng.random(),
+            rotation: rng.random(),
+            speed: 0.002,
+        }
+    }
+
+    pub fn position(&self) -> nalgebra::OPoint<f32, nalgebra::Const<2>> {
+        self.position
+    }
+
+    pub fn rotation(&self) -> nalgebra::Rotation<f32, 2> {
+        self.rotation
+    }
+}
+
+impl Food {
+    pub fn random(rng: &mut dyn Rng) -> Self {
+        Self {
+            position: rng.random(),
+        }
+    }
+
+    pub fn position(&self) -> nalgebra::OPoint<f32, nalgebra::Const<2>> {
+        self.position
     }
 }
