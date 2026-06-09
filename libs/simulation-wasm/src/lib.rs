d9d3ebc80c7e6b2acde445a580b9a6f7eb1aedcf
gev-hobosyan <hobosyangevorg2008@gmail.com>
2026-06-09 19:22:06 +0400

    Drawing animals and rotating them.

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -35,6 +35,7 @@ pub struct World {
 pub struct Animal {
     pub x: f32,
     pub y: f32,
+    pub rotation: f32,
 }
 
 impl From<&sim::World> for World {
@@ -50,6 +51,7 @@ impl From<&sim::Animal> for Animal {
         Self {
             x: animal.position().x,
             y: animal.position().y,
+            rotation: animal.rotation().angle(),
         }
     }
 }
